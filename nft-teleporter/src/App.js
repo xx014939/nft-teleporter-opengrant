de5a63ffa4885e305c3d8868c531f78e42c48602
xx014939 <57679526+xx014939@users.noreply.github.com>
2022-06-27 11:27:00 +0100

    Start page - almost finished

--- nft-teleporter/src/App.js
+++ nft-teleporter/src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
+import multimedia from './multimedia.png'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='lhs-container'>
+        <div className='lhs-inner'>
+           <div><img src={logo}/></div>
+           <div><a className='button' href='#'>BUTTON HERE</a></div>
+        </div>
+      </div>
+      <div className='rhs-container'>
+        <img src={multimedia}/>
+      </div>
     </div>
   );
 }
