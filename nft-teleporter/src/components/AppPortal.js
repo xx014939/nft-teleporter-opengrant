e810da43d62cbee2ede500aeb886931aa463773c
xx014939 <57679526+xx014939@users.noreply.github.com>
2022-06-28 21:51:58 +0100

    Step three

--- nft-teleporter/src/components/AppPortal.js
+++ nft-teleporter/src/components/AppPortal.js
@@ -7,6 +7,26 @@ function stepTwo() {
     document.querySelector('.lhs-inner-two').style.display = 'flex'
 }
 
+function stepThree () {
+    console.log('clicked')
+    document.querySelector('.lhs-inner-two').style.display = 'none'
+    document.querySelector('.lhs-inner-three').style.display = 'flex'
+
+    // Checkbox Functionality 
+    let checkboxArray = document.querySelectorAll('.checkbox-box')
+    checkboxArray.forEach(checkbox => checkbox.addEventListener('click', () => {
+        if (checkbox.classList.contains('checkbox-box-selected')) {
+            checkbox.classList.remove('checkbox-box-selected')
+        } else {
+            checkbox.classList.add('checkbox-box-selected')
+        }
+    }) )
+}
+
+function stepFour () {
+    console.log('step four')
+}
+
 function AppPortal() {
   return (
     <div>
@@ -18,9 +38,32 @@ function AppPortal() {
             <div><img src={logo}/></div>
             <div className='lhs-inner-two-text'>
                 <div><h2>NFT Data</h2></div>
-                <div><input label="Collection Name"></input></div>
-                <div><input label="Token Symbol"></input></div>
-                <div><a href='#' className='button'>BUTTON HERE</a></div>
+                <div><input className='input-field' placeholder='Collection Name' label="Collection Name"></input></div>
+                <div><input className='input-field' placeholder='Token Symbol' label="Token Symbol"></input></div>
+                <div><input className='input-field' placeholder='Number of NFTs inside Collection' type="number" id="quantity" name="quantity" min="1" max="5"></input></div>
+                <div><a href='#' className='button' onClick={stepThree}>Continue</a></div>
+            </div>
+        </div>
+        <div className='lhs-inner-three'>
+            <div><img src={logo}/></div>
+            <div className='lhs-inner-two-text'>
+                <div><h2>Digital Assets</h2></div>
+                <div>Which assets would you like to include?</div>
+                <div className='checkbox-container'>
+                    <div className='checkbox'>
+                        <div className='checkbox-box'></div>
+                        <div className='checkbox-text'>3D Digital Asset</div>
+                    </div>
+                    <div className='checkbox'>
+                        <div className='checkbox-box'></div>
+                        <div className='checkbox-text'>2D Digital Asset</div>
+                    </div>
+                    <div className='checkbox'>
+                        <div className='checkbox-box'></div>
+                        <div className='checkbox-text'>MP4 Video File</div>
+                    </div>
+                </div>
+                <div><a href='#' className='button' onClick={stepThree}>Continue</a></div>
             </div>
         </div>
     </div>
