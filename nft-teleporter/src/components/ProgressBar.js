875af3b6466c648dd9c9a4c83ac0da35bc68643f
xx014939 <57679526+xx014939@users.noreply.github.com>
2022-07-25 22:45:44 +0100

    After line element

--- nft-teleporter/src/components/ProgressBar.js
+++ nft-teleporter/src/components/ProgressBar.js
@@ -7,14 +7,17 @@ function ProgressBar () {
             <div className="progress-bar-element">1</div>
             <div className="progress-bar-text">Basic Info</div>
          </div>
+         <div style={{width: '100%', height: "2px", background: "#ffffff", marginTop: "30px"}}></div>
          <div className="progress-bar-step">
             <div className="progress-bar-element">2</div>
             <div className="progress-bar-text">Digital Assets</div>
          </div>
+         <div style={{width: '100%', height: "2px", background: "#ffffff", marginTop: "30px"}}></div>
          <div className="progress-bar-step">
             <div className="progress-bar-element">3</div>
-            <div className="progress-bar-text">Blockchains</div>
+            <div className="progress-bar-text">Blockchains kkk</div>
          </div>
+         <div style={{width: '100%', height: "2px", background: "#ffffff", marginTop: "30px"}}></div>
          <div className="progress-bar-step">
             <div className="progress-bar-element">4</div>
             <div className="progress-bar-text">Smart Contract</div>
