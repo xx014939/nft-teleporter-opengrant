daa6305596a83e4cc7b6d94ad2ae357c69106768
xx014939 <57679526+xx014939@users.noreply.github.com>
2022-07-25 22:28:49 +0100

    Main elements of progress bar

--- nft-teleporter/src/components/ProgressBar.js
+++ nft-teleporter/src/components/ProgressBar.js
@@ -1,3 +1,5 @@
+import '../styles/ProgressBar.css';
+
 function ProgressBar () {
  return (
      <div className="progress-bar-container">
@@ -8,6 +10,14 @@ function ProgressBar () {
          <div className="progress-bar-step">
             <div className="progress-bar-element">2</div>
             <div className="progress-bar-text">Digital Assets</div>
+         </div>
+         <div className="progress-bar-step">
+            <div className="progress-bar-element">3</div>
+            <div className="progress-bar-text">Blockchains</div>
+         </div>
+         <div className="progress-bar-step">
+            <div className="progress-bar-element">4</div>
+            <div className="progress-bar-text">Smart Contract</div>
          </div>
      </div>
  );
