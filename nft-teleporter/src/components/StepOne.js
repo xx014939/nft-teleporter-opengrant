47b843075553db1f29c26803423439514ad5f5c6
xx014939 <57679526+xx014939@users.noreply.github.com>
2022-07-27 20:56:43 +0100

    styling buttons (adding gradient border)

--- nft-teleporter/src/components/StepOne.js
+++ nft-teleporter/src/components/StepOne.js
@@ -17,9 +17,15 @@ function StepOne () {
                 <div><input type="number" id="quantity" name="quantity" min="1" max="5"></input></div>
             </div>
             <div className='step-one-input-container'>
-                <div>Do you want to Randomised Metada?</div>
-                <div>YES</div>
-                <div>NO</div>
+                <div className='step-one-input-label'>Do you want to Randomised Metada?</div>
+                <div className='step-one-buttons-container'>
+                    <div className='step-one-single-button-container'>
+                        <div>YES</div>
+                    </div>
+                    <div className='step-one-single-button-container'>
+                        <div>NO</div>
+                    </div>
+                </div>
             </div>
         </div>
     )
