060219d225088e96aeddf8726761d62ec5c5039d
xx014939 <57679526+xx014939@users.noreply.github.com>
2022-07-27 20:15:09 +0100

    beginning to style input boxes in create collection page

--- nft-teleporter/src/components/StepOne.js
+++ nft-teleporter/src/components/StepOne.js
@@ -5,11 +5,11 @@ function StepOne () {
         <div className='step-one-container'>
             <h2>Basic Info</h2>
             <div className='step-one-input-container'>
-                <div>Name (NFT Collection)</div>
-                <div><input placeholder='Name' label="Enter NFT Collection's Name"></input></div>
+                <div className='step-one-input-label'>Name (NFT Collection)</div>
+                <div><input type="text" placeholder='Name' label="Enter NFT Collection's Name"></input></div>
             </div>
             <div className='step-one-input-container'>
-                <div>Number of NFT's (inside of collection)</div>
+                <div className='step-one-input-label'>Number of NFT's (inside of collection)</div>
                 <div><input type="number" id="quantity" name="quantity" min="1" max="5"></input></div>
             </div>
             <div className='step-one-input-container'>
