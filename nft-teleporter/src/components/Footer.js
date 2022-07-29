37ec5df6d5587d7b7d909fb6ffaaafc65872cab3
xx014939 <57679526+xx014939@users.noreply.github.com>
2022-07-29 17:29:27 +0100

    Almost finished the footer

--- nft-teleporter/src/components/Footer.js
+++ nft-teleporter/src/components/Footer.js
@@ -6,7 +6,7 @@ function Footer () {
         <div className='footer-container'>
             <div className='footer-lhs'>
                 <div><img src={logo}/></div>
-                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed iaculis diam. Duis et blandit ligula, quis ullamcorper ante.</div>
+                <div className='footer-tag-line'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed iaculis diam. Duis et blandit ligula, quis ullamcorper ante.</div>
                 <div className='footer-socials-container'>
                     <div></div>
                     <div></div>
@@ -15,7 +15,12 @@ function Footer () {
                 </div>
             </div>
             <div className='footer-rhs'>
-
+                <div className='footer-link-columns'>
+                    <div>Lorem Ipsum</div>
+                    <a>Link One</a>
+                    <a>Link Two</a>
+                    <a>Link Three</a>
+                </div>
             </div>
         </div>
     )
