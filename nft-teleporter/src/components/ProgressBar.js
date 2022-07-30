d34f632bd6c3c7e5303c1a10c03332e40a7887bd
xx014939 <57679526+xx014939@users.noreply.github.com>
2022-07-30 16:01:41 +0100

    finish progress bar

--- nft-teleporter/src/components/ProgressBar.js
+++ nft-teleporter/src/components/ProgressBar.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import '../styles/ProgressBar.css';
+import tick from '../tick.svg'
 import StepOne from './StepOne.js';
 import StepTwo from './StepTwo';
 import StepThree from './StepThree';
 import StepFour from './StepFour'
 
+// Progress Bar Elements
+let stepArray = [1,2,3,4]
 
+// Current Step
 let counter = 1;
 
 function updateCount () {
@@ -16,6 +20,7 @@ function updateElement () {
     let elementArray = document.querySelectorAll('.progress-bar-element')
     for (let i = 0; i < counter; i++) {
         elementArray[i].classList.add('active-element')
+        stepArray[i - 1] = <img src={tick}/>
     }
 }
 
@@ -27,22 +32,22 @@ function ProgressBar () {
     <div>
         <div className="progress-bar-container">
          <div className="progress-bar-step">
-            <div className="progress-bar-element active-element">1</div>
+            <div className="progress-bar-element active-element">{stepArray[0]}</div>
             <div className="progress-bar-text">Basic Info</div>
          </div>
          <div style={{width: '100%', height: "2px", background: "#ffffff", marginTop: "30px"}}></div>
          <div className="progress-bar-step">
-            <div className="progress-bar-element">2</div>
+            <div className="progress-bar-element">{stepArray[1]}</div>
             <div className="progress-bar-text">Digital Assets</div>
          </div>
          <div style={{width: '100%', height: "2px", background: "#ffffff", marginTop: "30px"}}></div>
          <div className="progress-bar-step" style={{marginBottom: '16px', marginRight: '-8px', marginLeft: '-8px'}}>
-            <div className="progress-bar-element">3</div>
+            <div className="progress-bar-element">{stepArray[2]}</div>
             <div className="progress-bar-text">Blockchains</div>
          </div>
          <div style={{width: '100%', height: "2px", background: "#ffffff", marginTop: "30px"}}></div>
          <div className="progress-bar-step">
-            <div className="progress-bar-element">4</div>
+            <div className="progress-bar-element">{stepArray[3]}</div>
             <div className="progress-bar-text">Smart Contract</div>
          </div>
         </div>
