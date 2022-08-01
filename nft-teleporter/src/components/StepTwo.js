e3f4a52d6fe2abe8e4c19a6a66f16255e58ff944
xx014939 <57679526+xx014939@users.noreply.github.com>
2022-08-01 01:22:03 +0100

    show/hide upload boxes based on checkboxes ticked/unticked

--- nft-teleporter/src/components/StepTwo.js
+++ nft-teleporter/src/components/StepTwo.js
@@ -1,35 +1,50 @@
 import '../styles/StepTwo.css'
 import '../index.css'
+import React, { useState } from 'react';
 import threedFile from '../3dFile.svg'
+
+let checkboxArray  = [false, false, false]
 
 const selectBox = event => {
     event.currentTarget.classList.toggle('step-two-checkbox-icon-active');
+    checkboxArray[`${(event.currentTarget.id - 1)}`] = !checkboxArray[`${(event.currentTarget.id - 1)}`]
+
+    // Show/Hide Upload Boxes
+    const uploadBoxes = document.querySelectorAll('.upload-box')
+    for (let i = 0; i < uploadBoxes.length; i++) {
+        if (checkboxArray[i] === true) {
+            uploadBoxes[i].style.display = 'flex' 
+        } else {
+            uploadBoxes[i].style.display = 'none' 
+        }
+    }
   };
 
 function StepTwo () {
+
     return (
         <div className='step-two-container'>
             <div className='step-one-heading-container'><h2>Digital Assets</h2></div>
             <div className='page-container'>
                 <div className='step-two-checkbox-section'>
-                    <div className='step-two-label'>What Assets you have available?</div>
+                    <div className='step-two-label'>What Assets do you have currently available?</div>
                     <div className='step-two-checkbox-container'>
                         <div className='step-two-single-checkbox'>
-                            <div className='step-two-checkbox-icon' onClick={selectBox}></div>
+                            <div className='step-two-checkbox-icon' id="1" onClick={selectBox}></div>
                             <div className='step-two-checkbox-label'>3D Digital Asset</div>
                         </div>
                         <div className='step-two-single-checkbox'>
-                            <div className='step-two-checkbox-icon'onClick={selectBox}></div>
+                            <div className='step-two-checkbox-icon' id="2" onClick={selectBox}></div>
                             <div className='step-two-checkbox-label'>2D Digital Asset</div>
                         </div>
                         <div className='step-two-single-checkbox'>
-                            <div className='step-two-checkbox-icon'onClick={selectBox}></div>
+                            <div className='step-two-checkbox-icon' id="3" onClick={selectBox}></div>
                             <div className='step-two-checkbox-label'>MP4 Video File</div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className='page-container'>
+            <div className='page-container upload-box' style={{display: 'none'}}>
                 <div className='step-two-upload-asset-section'>
                     <div className='step-two-label'>Upload your 3D Digital Asset</div>
                     <div className='step-two-upload-container'>
