de6ffb016f7f78fc4f8d686d98f054d02f18f21b
xx014939 <57679526+xx014939@users.noreply.github.com>
2022-08-03 16:21:03 +0100

    New BG image + popup experiences modal

--- nft-teleporter/src/components/Experiences.js
+++ nft-teleporter/src/components/Experiences.js
@@ -1,15 +1,39 @@
 import TransparentHeader from "./TransparentHeader";
 import '../styles/Experiences.css';
-import vrBackground from '../vrBackground.svg'
+import bg from '../2049px.png'
+
+function SelectExperienceModal () {
+    return (
+        <div className="select-modal-container">
+            <div className="select-modal-header">
+
+            </div>
+            <div className="select-modal-body">
+                <div className="select-modal-experience">
+                    <div className="select-modal-experience-icon"></div>
+                    <div className="select-modal-experience-text"></div>
+                </div>
+                <div className="select-modal-experience">
+                    <div className="select-modal-experience-icon"></div>
+                    <div className="select-modal-experience-text"></div>
+                </div>
+                <div className="select-modal-experience">
+                    <div className="select-modal-experience-icon"></div>
+                    <div className="select-modal-experience-text"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 function Experiences () {
     return (
-            <div style={{backgroundImage: `url(${vrBackground})`, backgroundSize: 'cover', height: '100vh', backgroundRepeat: 'no-repeat', opacity: '0.5'}}>
+            <div style={{backgroundImage: `url(${bg})`, backgroundSize: 'cover', height: '100vh', backgroundRepeat: 'no-repeat', opacity: '0.5'}}>
             <div>
                 <TransparentHeader/>
             </div>
             <div>
-
+                <SelectExperienceModal/>
             </div>
         </div>
     )
