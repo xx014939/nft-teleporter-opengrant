2b41184fa92b1e5770df390697a7d3810d6aa31b
xx014939 <57679526+xx014939@users.noreply.github.com>
2022-08-03 14:49:33 +0100

    Adjust background image

--- nft-teleporter/src/components/Experiences.js
+++ nft-teleporter/src/components/Experiences.js
@@ -4,7 +4,7 @@ import vrBackground from '../vrBackground.svg'
 
 function Experiences () {
     return (
-            <div style={{backgroundImage: `url(${vrBackground})`, backgroundSize: '100% 100%', height: '100vh'}}>
+            <div style={{backgroundImage: `url(${vrBackground})`, backgroundSize: 'cover', height: '100vh', backgroundRepeat: 'no-repeat', opacity: '0.5'}}>
             <div>
                 <TransparentHeader/>
             </div>
