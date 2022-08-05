1ae51de50d068981564dc22e927dade5f9b861de
xx014939 <57679526+xx014939@users.noreply.github.com>
2022-08-05 01:23:22 +0100

    Setup Three Fiber Library + Demo

--- nft-teleporter/src/index.js
+++ nft-teleporter/src/index.js
@@ -10,6 +10,8 @@ import Experiences from './components/Experiences';
 import VRExperience from './components/VRExperience';
 import ARExperience from './components/ARExperience';
 import TwoDExperience from './components/TwoDExperience';
+import ThreeJS from './components/ThreeJS';
+import { Canvas, useFrame } from '@react-three/fiber';
 import {
   BrowserRouter,
   Routes,
@@ -38,6 +40,16 @@ root.render(
       <Route path="vr-experience" element={<VRExperience />} />
       <Route path="ar-experience" element={<ARExperience />} />
       <Route path="2d-experience" element={<TwoDExperience />} />
+      <Route path="threejs" element={
+        <div style={{height: '100vh'}}>
+        <Canvas>
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <ThreeJS position={[-1.2, 0, 0]}/>
+          <ThreeJS position={[1.2, 0, 0]} />
+        </Canvas>
+        </div>
+      } />
     </Routes>
   </BrowserRouter>
 );
