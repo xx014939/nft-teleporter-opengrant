233c82f023fb76f9c9eed90f91ff1905772a29b0
xx014939 <57679526+xx014939@users.noreply.github.com>
2022-08-06 15:35:11 +0100

    Beginning account page HTML structure

--- nft-teleporter/src/components/AccountPage.js
+++ nft-teleporter/src/components/AccountPage.js
@@ -1,9 +1,33 @@
 import Header from "./Header";
+import '../styles/AccountPage.css';
 
 function AccountPage () {
  return (
      <div>
-         <Header/>
+         <div><Header/></div>
+         <div className="account-page-container">
+            <div className="account-page-lhs-container">
+                <div className="account-page-lhs-back-button">
+                    <div>Icon</div>
+                    <div>BACK</div>
+                </div>
+                <div className="account-page-lhs-progress-bar">
+                    <div className="account-page-lhs-progress-bar-element">
+                        <div>Icon</div>
+                        <div>General Info</div>
+                    </div>
+                    <div className="account-page-lhs-progress-bar-element">
+                        <div>Icon</div>
+                        <div>Security</div>
+                    </div>
+                    <div className="account-page-lhs-progress-bar-element">
+                        <div>Icon</div>
+                        <div>NFT Collections</div>
+                    </div>
+                </div>
+            </div>
+            <div className="account-page-rhs-container"></div>
+         </div>
      </div>
  );
 }
