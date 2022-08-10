49443c3a066737b347865c08df8bcdbdd55d5d1c
xx014939 <57679526+xx014939@users.noreply.github.com>
2022-08-10 20:20:44 +0100

    Header links

--- nft-teleporter/src/components/Header.js
+++ nft-teleporter/src/components/Header.js
@@ -7,6 +7,15 @@ function Header () {
             <div className='header-logo'>
                 <img src={logo} />
             </div>
+            <div className='header-linklist'>
+                <div>About Us</div>
+                <div>How It Works</div>
+            </div>
+            <div className='header-create-collection-container'>
+                <div className='header-create-collection-inner'>
+                    <a href='#'>Create Collection</a>
+                </div>
+            </div>
         </div>
     )
 }
