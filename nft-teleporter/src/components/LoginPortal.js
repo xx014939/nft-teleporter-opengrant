128f87157a7c4b4fd66c9916c09892cd13fe23b7
xx014939 <57679526+xx014939@users.noreply.github.com>
2022-08-13 19:11:57 +0100

    Fix createRoot duplicate error by using usestate hook and short circuits instead of conditional rendering

--- nft-teleporter/src/components/LoginPortal.js
+++ nft-teleporter/src/components/LoginPortal.js
@@ -1,113 +1,66 @@
 import logo from '../logo.svg';
 import multimedia from '../multimedia.svg';
-import rhsImage from '../rhsImage.svg';
 import starryBG from '../starryBG.svg'
 import '../App.css';
-import ReactDOM from 'react-dom/client';
+import React, { useState } from 'react';
 
-function RegisterOrLogin(props) {
-    return (
-        <div className='lhs-inner'>
-            <div><img alt='NFT Teleporter Logo' src={logo}/></div>
-            <div className="button-container">
-                <div><a onClick={loginView} className='button' href='#'>Login</a></div>
-                <div><a onClick={registerView} className='button' href='#'>Register</a></div>
-            </div>
-        </div>
-    )
-  }
-  
-  function ShowLogin(props) {
-    return (
-        <div className='lhs-inner-two'>
-            <div><img alt='NFT Teleporter Logo' src={logo}  style={{marginLeft: '-20px'}}/></div>
-            <div className='lhs-inner-two-text'>
-                <div><h2>Login</h2></div>
-                <div><input className='input-field' placeholder='Username' label="Username"></input></div>
-                <div><input className='input-field' placeholder='Password' label="Password"></input></div>
-                <div><a href='/account' className='button' onClick={accountView}>Login</a></div>
-            </div>
-        </div>
-    )
-  }
-  
-  function ShowRegister(props) {
-    return (
-      <div className='lhs-inner-three'>
-        <div><img alt='NFT Teleporter Logo' src={logo} style={{marginLeft: '-40px'}}/></div>
-        <div className='lhs-inner-three-text'>
-            <div><h2>Register</h2></div>
-            <div><input className='input-field' placeholder='Email' label="Email"></input></div>
-            <div><input className='input-field' placeholder='Username' label="Username"></input></div>
-            <div><input className='input-field' placeholder='Password' label="Password"></input></div>
-            <div><a href='/account' className='button' onClick={accountView}>Register</a></div>
-        </div>
-      </div>
-    )
-  }
-  
-  function LoginPortal(props) {
-    const stateCount = props.stateCount;
-    if(stateCount === 1) {
-        return (
-          <>
-          <div className="container" style={{backgroundImage: `url(${starryBG})`}}>
-            <div className='lhs-container'>
-            <RegisterOrLogin/>
-            </div>
-            <div className='rhs-container'>
-              <img src={multimedia}/>
-            </div>
-          </div>
-          </>
-        )
-    } else if (stateCount === 2) {
+
+  function LoginPortal() {
+    const [stepNumber, setStepNumber] = useState(1);
+
+    function RegisterOrLogin(props) {
       return (
-        <>
-        <div className="container" style={{backgroundImage: `url(${starryBG})`}}>
-          <div className='lhs-container'>
-          <ShowLogin/>
+          <div className='lhs-inner'>
+              <div><img alt='NFT Teleporter Logo' src={logo}/></div>
+              <div className="button-container">
+                  <div><a onClick={() => {setStepNumber(2)}} id="login" className='button' href='#'>Login</a></div>
+                  <div><a onClick={() => {setStepNumber(3)}} id="register" className='button' href='#'>Register</a></div>
+              </div>
           </div>
-          <div className='rhs-container'>
-            <img src={multimedia}/>
-          </div>
-        </div>
-        </>
-      )
-    } else {
-      return (
-        <>
-        <div className="container" style={{backgroundImage: `url(${starryBG})`}}>
-          <div className='lhs-container'>
-          <ShowRegister/>
-          </div>
-          <div className='rhs-container'>
-            <img src={multimedia}/>
-          </div>
-        </div>
-        </>
       )
     }
+    
+    function ShowLogin() {
+      return (
+          <div className='lhs-inner-two'>
+              <div><img alt='NFT Teleporter Logo' src={logo}  style={{marginLeft: '-20px'}}/></div>
+              <div className='lhs-inner-two-text'>
+                  <div><h2>Login</h2></div>
+                  <div><input className='input-field' placeholder='Username' label="Username"></input></div>
+                  <div><input className='input-field' placeholder='Password' label="Password"></input></div>
+                  <div><a href='/account' className='button'>Login</a></div>
+              </div>
+          </div>
+      )
+    }
+    
+    function ShowRegister() {
+      return (
+        <div className='lhs-inner-three'>
+          <div><img alt='NFT Teleporter Logo' src={logo} style={{marginLeft: '-40px'}}/></div>
+          <div className='lhs-inner-three-text'>
+              <div><h2>Register</h2></div>
+              <div><input className='input-field' placeholder='Email' label="Email"></input></div>
+              <div><input className='input-field' placeholder='Username' label="Username"></input></div>
+              <div><input className='input-field' placeholder='Password' label="Password"></input></div>
+              <div><a href='/account' className='button'>Register</a></div>
+          </div>
+        </div>
+      )
+    }
+    
+    return (
+      <div className="container" style={{backgroundImage: `url(${starryBG})`}}>
+      <div className='lhs-container'>
+        {stepNumber === 1 && <RegisterOrLogin/>}
+        {stepNumber === 2 && <ShowLogin/>}
+        {stepNumber === 3 && <ShowRegister/>}
+      </div>
+      <div className='rhs-container'>
+        <img src={multimedia}/>
+      </div>
+    </div>
+    )
   }
-  
-
-  const container = document.getElementById('root')
-  const root = ReactDOM.createRoot(container);
-  
-  
-  function loginView() {
-    // ReactDOM.render(<LoginPortal stateCount={2}/>, document.getElementById('root'));
-    root.render(<LoginPortal stateCount={2}/>)
-  }
-  
-  function registerView () {
-    // ReactDOM.render(<LoginPortal stateCount={3}/>, document.getElementById('root'));
-    root.render(<LoginPortal stateCount={3}/>)
-  }
-  
-  function accountView () {
-    console.log('Go to account')
-  }
-  
 
 export default LoginPortal;
