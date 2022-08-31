038e2d52e7b1dc05ad6363d4892e95c2a798a065
xx014939 <57679526+xx014939@users.noreply.github.com>
2022-08-31 21:20:09 +0100

    Add SOLC for solidity compilation

--- nft-teleporter/src/components/GenerateETHWallet.js
+++ nft-teleporter/src/components/GenerateETHWallet.js
@@ -1,12 +1,21 @@
 import Web3 from 'web3';
+import * as wrapper from 'solc/wrapper';
 
 const web3 = new Web3(Web3.givenProvider || "http://localhost:3000");
+const web3Two = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/f6ea9a5670444f3b8f2221aa4d57149b"))
+
+function connectRPC () {
+    console.log('RPC')
+    console.log('first -->',web3.eth.accounts)
+    console.log('next -->', web3Two.eth.accounts)
+}
 
 
 function GenerateETHWallet () {
     return (
         <div>
             <button onClick={() => {console.log(web3.eth.accounts.create())}}>GENERATE ETH WALLET</button>
+            <button onClick={() => {connectRPC()}}>CONNECT TO RPC</button>
         </div>
     )
 }
