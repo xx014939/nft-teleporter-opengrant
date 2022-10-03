d94aa2aa7a3ab1c0e351ac5529e81553675604e4
xx014939 <57679526+xx014939@users.noreply.github.com>
2022-10-03 15:07:21 +0100

    update smart contract

--- nft-teleporter/src/smart-contracts/boilerplate.sol
+++ nft-teleporter/src/smart-contracts/boilerplate.sol
@@ -1,25 +1,27 @@
-// contracts/GameItem.sol
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.3;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract GameItem is ERC721URIStorage {
-    using Counters for Counters.Counter;
+contract FactoryNFT is ERC721URIStorage { 
+
+    using Counters for Counters.Counter; 
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("GameItem", "ITM") {}
+    constructor() ERC721("NFT Contract", "NFC") {
+    }
 
-    function awardItem(address player, string memory tokenURI)
-        public
-        returns (uint256)
-    {
-        uint256 newItemId = _tokenIds.current();
-        _mint(player, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-
+    function createToken(string memory tokenURI) public returns (uint) {
         _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+
+        if (newItemId < 4) {
+            _mint(msg.sender, newItemId);
+            _setTokenURI(newItemId, tokenURI);
+        }
+
         return newItemId;
     }
 }
