31edb9783db0741e294fed1ff3db3a9d24732bd3
xx014939 <57679526+xx014939@users.noreply.github.com>
2022-11-07 20:43:54 +0000

    Pass data in using useEffect hook

--- nft-teleporter/src/components/TwoDExperience.js
+++ nft-teleporter/src/components/TwoDExperience.js
@@ -5,17 +5,23 @@ import React, { useState, useEffect  } from 'react';
 
 function TwoDExperience () {
     async function retrieveImage() {
-
         let imageHash = await axios.post('http://localhost:5000/users/image', {username: "44"})
-        console.log('IMAGE HASH -->', imageHash)
         return imageHash
     }
 
-    let newSRC = retrieveImage()
-
     useEffect(() => {
-        document.querySelector('.experience-image').src = `https://yourmetaworld.mypinata.cloud/${newSRC}`
-    }, newSRC)
+        // declare the async data fetching function
+        const fetchData = async () => {
+          const data = await retrieveImage();
+          console.log('DATA -->', data.data.imageHash)
+          document.querySelector('.experience-image').src = `https://yourmetaworld.mypinata.cloud/ipfs/${data.data.imageHash}`
+          return data;
+        }
+      
+        // call the function
+        const result = fetchData().catch(console.error);
+      
+      }, [])
 
     return (
         <div className='twod-experience-container' style={{backgroundImage: `url(${TwoDBG})`, backgroundSize: 'cover', height: '100vh', backgroundRepeat: 'no-repeat'}}>
