89e8fc5ce6af980c34aadb97717f1af8fad5efe0
xx014939 <57679526+xx014939@users.noreply.github.com>
2022-11-07 20:17:53 +0000

    cleanup

--- nft-teleporter/src/components/TwoDExperience.js
+++ nft-teleporter/src/components/TwoDExperience.js
@@ -2,35 +2,14 @@ import TwoDBG from '../assets/twod-bg.svg'
 import PlaceHolder from '../assets/placeholder.png'
 import axios from 'axios';
 
-function getCookie(cookieName) {
-    let cookieValue = document.cookie
-    .split('; ')
-    .find((row) => row.startsWith(`${cookieName}=`))
-    ?.split('=')[1];
-    console.log(cookieValue)
-
-    return cookieValue;
-}
-
-
 function TwoDExperience () {
     async function retrieveImage() {
-
-        let jwtToken = getCookie('jwt')
-        const config = {
-            headers: { 
-                'Authorization': `Bearer ${jwtToken}` 
-            }
-        }
-
-        console.log('THE JWT TOKEN IS -->', jwtToken)
 
         let imageURL = await axios.post('http://localhost:5000/users/image', {username: "44"})
         console.log('IMAGE URL -->',imageURL)
     }
 
     retrieveImage()
-    console.log('hereeee')
 
     return (
         <div className='twod-experience-container' style={{backgroundImage: `url(${TwoDBG})`, backgroundSize: 'cover', height: '100vh', backgroundRepeat: 'no-repeat'}}>
