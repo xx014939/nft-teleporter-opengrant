a4e77a20f7e0b3c101811a5f5f4bfb01aa1e662e
xx014939 <57679526+xx014939@users.noreply.github.com>
2022-11-07 20:23:55 +0000

    pass hash url into scene

--- nft-teleporter/src/components/TwoDExperience.js
+++ nft-teleporter/src/components/TwoDExperience.js
@@ -1,19 +1,25 @@
 import TwoDBG from '../assets/twod-bg.svg'
 import PlaceHolder from '../assets/placeholder.png'
 import axios from 'axios';
+import React, { useState, useEffect  } from 'react';
 
 function TwoDExperience () {
     async function retrieveImage() {
 
         let imageHash = await axios.post('http://localhost:5000/users/image', {username: "44"})
-        console.log('IMAGE HASH -->',imageHash)
+        console.log('IMAGE HASH -->', imageHash)
+        return imageHash
     }
 
-    retrieveImage()
+    let newSRC = retrieveImage()
+
+    useEffect(() => {
+        document.querySelector('.experience-image').src = `https://yourmetaworld.mypinata.cloud/${newSRC}`
+    }, newSRC)
 
     return (
         <div className='twod-experience-container' style={{backgroundImage: `url(${TwoDBG})`, backgroundSize: 'cover', height: '100vh', backgroundRepeat: 'no-repeat'}}>
-            <img id='imageTag' src={PlaceHolder}/>
+            <img className='experience-image' id='imageTag' src={PlaceHolder}/>
         </div>
     )
 }
